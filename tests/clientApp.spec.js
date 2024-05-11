702c379b93c6e7f775a3ae516e2394ddfc8116e9
Ana Paula Lima <paulinhasjlima@gmail.com>
2024-05-11 14:20:18 -0300

    add product to cart

--- tests/clientApp.spec.js
+++ tests/clientApp.spec.js
@@ -5,14 +5,23 @@ const {test, expect} =  require('@playwright/test');
 test('Client App Login', async ({page})=>
 {
     const email = "paulinhasjlima+tests@gmail.com";
-    const products = page.locator(".card-body b");
+    const products = page.locator(".card-body");
+    const productName = 'ZARA COAT 3';
     
 
     await page.goto("https://rahulshettyacademy.com/client"); 
     await page.locator("#userEmail").fill(email);
     await page.locator("#userPassword").fill("Testsplay24*");
     await page.locator("[value='Login']").click();
-    //await page.waitForLoadState('networkidle');    dosent work, learn another option for the call request 
-    await products.first().waitFor();
+    await page.waitForLoadState('networkidle');    //dosent work, learn another option for the call request, possibility - products.first().waitFor();
+    const titiles = await products.locator("b").allTextContents();
+    const count = await products.count();
+    for (let i=0; i < count; ++i)
+    {
+      if (await products.nth(i).locator("b").textContent() === productName){
+            await products.nth(i).locator("[class='btn w-10 rounded']").click();
+            break;
+      }  
+    }
     
 });
